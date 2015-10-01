0d2faef602cf4b969ffb9cce7b0a8b4779f24c3f
KoaGex <koagex@openmailbox.org>
2015-10-01 22:46:53 +0200

    MusicFile - refactored, now using method references.

--- ImageBattleFx/src/main/java/org/imagebattle/MusicFile.java
+++ ImageBattleFx/src/main/java/org/imagebattle/MusicFile.java
@@ -2,11 +2,11 @@ package org.imagebattle;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.function.Function;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.farng.mp3.MP3File;
 import org.farng.mp3.TagException;
+import org.farng.mp3.id3.AbstractID3v2;
 import org.farng.mp3.id3.ID3v1;
 
 /**
@@ -17,7 +17,7 @@ import org.farng.mp3.id3.ID3v1;
  *
  */
 public class MusicFile extends MP3File {
-    private static Logger log = LogManager.getLogger();
+    private static final String UNKNOWN = "";
 
     /**
      * Constructor replacement to avoid handling the exceptions.
@@ -38,38 +38,26 @@ public class MusicFile extends MP3File {
     }
 
     String getAlbum() {
-	if (hasID3v1Tag()) {
-	    ID3v1 id3v1Tag = getID3v1Tag();
-	    log.debug("album {}   albumTitle {}", id3v1Tag.getAlbum(), id3v1Tag.getAlbumTitle());
-	    return id3v1Tag.getAlbum();
-	}
-	if (hasID3v2Tag()) {
-	    return getID3v2Tag().getAlbumTitle();
-	}
-	return "";
+	return getTagValue(ID3v1::getAlbum, AbstractID3v2::getAlbumTitle);
     }
 
     String getTitle() {
-	if (hasID3v1Tag()) {
-	    ID3v1 id3v1Tag = getID3v1Tag();
-	    log.debug("title {}   songTitle {}", id3v1Tag.getTitle(), id3v1Tag.getSongTitle());
-	    return id3v1Tag.getTitle();
-	}
-	if (hasID3v2Tag()) {
-	    return getID3v2Tag().getSongTitle();
-	}
-	return "";
+	return getTagValue(ID3v1::getTitle, AbstractID3v2::getSongTitle);
     }
 
     String getArtist() {
+	return getTagValue(ID3v1::getArtist, AbstractID3v2::getLeadArtist);
+    }
+
+    private String getTagValue(Function<ID3v1, String> v1, Function<AbstractID3v2, String> v2) {
 	if (hasID3v1Tag()) {
 	    ID3v1 id3v1Tag = getID3v1Tag();
-	    log.debug("artist {0}   leadArtist {1}", id3v1Tag.getArtist(), id3v1Tag.getLeadArtist());
-	    return id3v1Tag.getArtist();
+	    return v1.apply(id3v1Tag);
 	}
 	if (hasID3v2Tag()) {
-	    return getID3v2Tag().getLeadArtist();
+	    AbstractID3v2 id3v2Tag = getID3v2Tag();
+	    return v2.apply(id3v2Tag);
 	}
-	return "";
+	return UNKNOWN;
     }
 }
