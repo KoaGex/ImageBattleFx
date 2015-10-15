1e1e3e403d5c925d47ab470db520a4daa864f60f
KoaGex <koagex@openmailbox.org>
2015-10-15 21:19:16 +0200

    MinimumDegreeCandidateChooser - throw out functionaljava.

--- ImageBattleFx/src/main/java/org/imagebattle/chooser/MinimumDegreeCandidateChooser.java
+++ ImageBattleFx/src/main/java/org/imagebattle/chooser/MinimumDegreeCandidateChooser.java
@@ -8,9 +8,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.imagebattle.TransitiveDiGraph2;
 
-import fj.F;
-import fj.data.vector.V;
-import fj.data.vector.V2;
 import javafx.util.Pair;
 
 public class MinimumDegreeCandidateChooser extends ACandidateChooser {
@@ -23,18 +20,15 @@ public class MinimumDegreeCandidateChooser extends ACandidateChooser {
     @Override
     Pair<File, File> doGetNextCandidates() {
 
-	Function<Pair<File, File>, V2<File>> pairToVector = pair -> V.v(pair.getKey(), pair.getValue());
-	F<File, Integer> fileToDegree = file -> graph2.inDegreeOf(file) + graph2.outDegreeOf(file);
-	Function<V2<File>, Integer> toDegreeSum = v2 -> {
-	    V2<Integer> v2Degree = v2.map(fileToDegree);
-	    return v2Degree._1() + v2Degree._2();
-	};
+	Function<File, Integer> fileToDegree = file -> graph2.inDegreeOf(file) + graph2.outDegreeOf(file);
 
-	Function<Pair<File, File>, Integer> filePairToDegreeSum = pairToVector.andThen(toDegreeSum);
+	Function<Pair<File, File>, Integer> filePairToDegreeSum = pair -> {
+	    return fileToDegree.apply(pair.getKey()) + fileToDegree.apply(pair.getValue());
+	};
 
 	java.util.stream.Stream<Pair<File, File>> candidateStream = getCandidateStream();
 	Pair<File, File> result = candidateStream.sorted(Comparator.comparing(filePairToDegreeSum)).findFirst().get();
-	log.debug("minDegreeSum:"+ filePairToDegreeSum.apply(result));
+	log.debug("minDegreeSum:" + filePairToDegreeSum.apply(result));
 	return result;
     }
 
