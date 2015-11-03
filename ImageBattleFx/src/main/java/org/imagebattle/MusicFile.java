7251c8202bbd2d25d5c16c2a10dcc6068aedd0cc
KoaGex <koagex@openmailbox.org>
2015-11-03 21:41:10 +0100

    MusicFile - add getLength and PMD fixes.

--- ImageBattleFx/src/main/java/org/imagebattle/MusicFile.java
+++ ImageBattleFx/src/main/java/org/imagebattle/MusicFile.java
@@ -2,7 +2,12 @@ package org.imagebattle;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 import java.util.function.Function;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,63 +15,97 @@ import org.farng.mp3.MP3File;
 import org.farng.mp3.TagException;
 import org.farng.mp3.id3.AbstractID3v2;
 import org.farng.mp3.id3.ID3v1;
+import org.tritonus.share.sampled.file.TAudioFileFormat;
 
 /**
- * Simplifies reading tags like band, album or title. Currently only works for
- * MP3 but maybe support for OGG will be added later.
+ * Simplifies reading tags like band, album or title. Currently only works for MP3 but maybe support for OGG will be
+ * added later.
  * 
  * @author KoaGex
  *
  */
 final class MusicFile extends MP3File {
-    private static final Logger log = LogManager.getLogger();
-    private static final String UNKNOWN = "";
+	private static final Logger LOG = LogManager.getLogger();
+	private static final String UNKNOWN = "";
 
-    /**
-     * Constructor replacement to avoid handling the exceptions.
-     */
-    static MusicFile create(File file) {
-	try {
-	    return new MusicFile(file);
-	} catch (Exception e) {
-	    if (log.isWarnEnabled()) {
-		log.warn("file:" + file.getAbsolutePath(), e);
-	    }
-	    return new MusicFile();
+	/**
+	 * Constructor replacement to avoid handling the exceptions.
+	 */
+	protected static MusicFile create(File file) {
+		try {
+			return new MusicFile(file);
+		} catch (IOException | TagException e) {
+			if (LOG.isWarnEnabled()) {
+				LOG.warn("file:" + file.getAbsolutePath(), e);
+			}
+			return new MusicFile();
+		}
 	}
-    }
 
-    /**
-     * {@link #create(File)} should be used from the outside.
-     */
-    private MusicFile(File file) throws IOException, TagException {
-	super(file);
-    }
-
-    private MusicFile() {
-    }
-
-    String getAlbum() {
-	return getTagValue(ID3v1::getAlbum, AbstractID3v2::getAlbumTitle);
-    }
-
-    String getTitle() {
-	return getTagValue(ID3v1::getTitle, AbstractID3v2::getSongTitle);
-    }
-
-    String getArtist() {
-	return getTagValue(ID3v1::getArtist, AbstractID3v2::getLeadArtist);
-    }
-
-    private String getTagValue(Function<ID3v1, String> v1, Function<AbstractID3v2, String> v2) {
-	if (hasID3v1Tag()) {
-	    ID3v1 id3v1Tag = getID3v1Tag();
-	    return v1.apply(id3v1Tag);
+	/**
+	 * {@link #create(File)} should be used from the outside.
+	 */
+	private MusicFile(File file) throws IOException, TagException {
+		super(file);
 	}
-	if (hasID3v2Tag()) {
-	    AbstractID3v2 id3v2Tag = getID3v2Tag();
-	    return v2.apply(id3v2Tag);
+
+	private MusicFile() {
+		super();
 	}
-	return UNKNOWN;
-    }
+
+	String getAlbum() {
+		Function<ID3v1, String> v1Album = ID3v1::getAlbum;
+		Function<AbstractID3v2, String> v2Album = AbstractID3v2::getAlbumTitle;
+		return getTagValue(v1Album, v2Album);
+	}
+
+	String getTitle() {
+		Function<ID3v1, String> v1Title = ID3v1::getTitle;
+		Function<AbstractID3v2, String> v2Title = AbstractID3v2::getSongTitle;
+		return getTagValue(v1Title, v2Title);
+	}
+
+	String getArtist() {
+		Function<ID3v1, String> v1Artist = ID3v1::getArtist;
+		Function<AbstractID3v2, String> v2Artist = AbstractID3v2::getLeadArtist;
+		return getTagValue(v1Artist, v2Artist);
+	}
+
+	private String getTagValue(Function<ID3v1, String> v1, Function<AbstractID3v2, String> v2) {
+		String result = UNKNOWN;
+		if (hasID3v1Tag()) {
+			ID3v1 id3v1Tag = getID3v1Tag();
+			result = v1.apply(id3v1Tag);
+		} else if (hasID3v2Tag()) {
+			AbstractID3v2 id3v2Tag = getID3v2Tag();
+			result = v2.apply(id3v2Tag);
+		}
+		return result;
+	}
+
+	String getLength() {
+		File mp3file = getMp3file();
+		AudioFileFormat fileFormat;
+		try {
+			fileFormat = AudioSystem.getAudioFileFormat(mp3file);
+			if (fileFormat instanceof TAudioFileFormat) {
+				Map<?, ?> properties = ((TAudioFileFormat) fileFormat).properties();
+				String key = "duration";
+				Long microseconds = (Long) properties.get(key);
+				int mili = (int) (microseconds / 1000);
+				int sec = (mili / 1000) % 60;
+				int min = (mili / 1000) / 60;
+				LOG.trace("time = {} : {}", min, sec);
+				return min + ":" + sec;
+			} else {
+				throw new UnsupportedAudioFileException();
+			}
+		} catch (UnsupportedAudioFileException | IOException | NullPointerException e) {
+			LOG.catching(e);
+			e.printStackTrace();
+		}
+
+		return "";
+
+	}
 }
