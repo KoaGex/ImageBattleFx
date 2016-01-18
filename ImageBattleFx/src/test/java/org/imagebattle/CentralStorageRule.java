6aec7d51a6ff21370634509743e05ce748190ffc
KoaGex <koagex@openmailbox.org>
2016-01-18 23:49:25 +0100

    CentralStorageRule - created class.

---
+++ ImageBattleFx/src/test/java/org/imagebattle/CentralStorageRule.java
@@ -0,0 +1,40 @@
+package org.imagebattle;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.rules.ExternalResource;
+
+public class CentralStorageRule extends ExternalResource {
+
+  static final String IGNORE_FILE_TEST = "ignoreFile_test.csv";
+  static final String GRAPH_FILE_TEST = "graphFile_test.csv";
+  static final String SQLITE_FILE_TEST = "sqliteFile_test.csv";
+  private Path ignorePath = Paths.get(System.getProperty("user.home"), IGNORE_FILE_TEST);
+  private Path graphPath = Paths.get(System.getProperty("user.home"), GRAPH_FILE_TEST);
+  private Path sqlitePath = Paths.get(System.getProperty("user.home"), SQLITE_FILE_TEST);
+  private CentralStorage centralStorage;
+
+  @Override
+  protected void before() throws Throwable {
+    centralStorage = new CentralStorage(IGNORE_FILE_TEST, GRAPH_FILE_TEST, SQLITE_FILE_TEST);
+  }
+
+  @Override
+  protected void after() {
+    try {
+      Files.deleteIfExists(graphPath);
+      Files.deleteIfExists(ignorePath);
+      Files.deleteIfExists(sqlitePath);
+    } catch (IOException e) {
+      throw new UncheckedIOException("careful: some files were maybe not deleted", e);
+    }
+  }
+
+  public CentralStorage centralStorage() {
+    return centralStorage;
+  }
+}
