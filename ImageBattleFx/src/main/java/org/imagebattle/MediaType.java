da146e8955a94f5ff7ad76120135877cee45cb81
KoaGex <koagex@openmailbox.org>
2016-03-07 20:42:40 +0100

    MediaType - integrate file type checkers.

--- ImageBattleFx/src/main/java/org/imagebattle/MediaType.java
+++ ImageBattleFx/src/main/java/org/imagebattle/MediaType.java
@@ -1,9 +1,32 @@
 package org.imagebattle;
 
+import java.io.File;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
 /**
  * @author Besitzer
  *
  */
 public enum MediaType {
-  IMAGE, MUSIC;
+  IMAGE(".*\\.(BMP|GIF|JPEG|JPG|PNG)"), //
+  MUSIC(".*\\.(MP3|OGG)");
+
+  Predicate<File> predicate;
+
+  private MediaType(String regex) {
+    predicate = createFileRegexChecker(regex);
+  }
+
+  public boolean matches(File file) {
+    return predicate.test(file);
+  }
+
+  private static Predicate<File> createFileRegexChecker(String regex) {
+    Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+    return file -> {
+      return pattern.matcher(file.getName()).matches();
+    };
+  }
+
 }
